a43d76ff2815ec4214cc26f640470a619796129b
Jeffrey Steward <nulifier@gmail.com>
2018-04-16 23:21:44 -0600

    Setup the library

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import ConanFile, tools
 import os
 
 class LibnameConan(ConanFile):
-    name = "wink"
+    name = "Wink-Signals"
     version = "20180415"
     commit_id = "c29b1168044bf18e198816a64b03020462291300"
     url = "https://github.com/bincrafters/conan-libname"
@@ -14,7 +14,7 @@ class LibnameConan(ConanFile):
     no_copy_source = True
 
     # Indicates License type of the packaged library
-    license = "MIT"
+    license = "zlib"
 
     # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
@@ -24,17 +24,17 @@ class LibnameConan(ConanFile):
 
     def source(self):
         source_url = "https://github.com/miguelmartin75/Wink-Signals"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.commit_id))
-        extracted_dir = self.name + "-" + self.version
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.commit_id))
+        extracted_dir = self.name + "-" + self.commit_id
 
         #Rename to "source_folder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
 
     def package(self):
-        include_folder = os.path.join(self.source_subfolder, "include")
+        include_folder = os.path.join(self.source_subfolder, "wink")
         self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
+        self.copy(pattern="*", dst="include/wink", src=include_folder)
 
 
     def package_id(self):
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,6 +1,13 @@
-// #include <cpprest/json.h>
+#include <wink/slot.hpp>
+#include <wink/signal.hpp>
+
+struct Event {
+    int a;
+}
 
 int main()
 {
-    // const auto parsed_value = web::json::value::parse(U("-22"));
+    wink::slot<void (int)> mySlot;
+    wink::signal<wink::slot<void (int)> mySignal;
+    wink::event_queue<Event> myEventQueue;
 }
